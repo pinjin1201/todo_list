610ca00889c72eb68a8848df249169acaf373664
pinjin1201 <knowledge84121@gmail.com>
2022-03-02 13:52:34 +0800

    add add todo item function

--- js/index.js
+++ js/index.js
@@ -6,20 +6,22 @@ $(function() {
   const $ul = $('ul')
   const $title = $('.title-text')
   const $amount = $('.title-text .amount')
-  const item = {
-    one: 'Buy Milk',
-    two: 'Pay Phone Bill',
-    three: 'Eat Vegetable'
-  }
+  const $start = $('.start-btn')
+  const $add = $('.add')
+  const $input = $('.add input')
+  const $addBtn = $('.add .add-btn')
+  let item = ['Buy Milk', 'Pay Phone Bill', 'Eat Vegetable']
 
-  // render and show todo item
-  $ul.html(function() {
+  // render todo item 
+  function renderTodoList(data) {
     let content = ''
-    for (let i in item) {
-      content += `<li>${item[i]}</li>`
-    }
+    data.forEach(li => {
+      content += `<li>${li}</li>`
+    })
     return content
-  })
+  }
+  // render and show todo item
+  $ul.html(renderTodoList(item))
   const $li = $('ul li')
   $li.hide().each(function (index) {
     $(this).delay(500 * index).fadeIn(700)
@@ -31,6 +33,22 @@ $(function() {
     marginLeft: '+=15'
   }, 1700, function() {
     $(this).css('margin-left', '-=15')
+  })
+
+  // start to add item
+  $start.on('click', function (event) {
+    $(this).hide()
+    $add.slideDown(700)
+  })
+
+  // add item to ToDo list
+  $addBtn.on('click', function () {
+    const value = $input.val()
+    if (value.length > 25) return alert('字數請控制在 25 以內')
+    if (value.trim()) {
+      item.push(value)
+      $ul.html(renderTodoList(item))
+    }
   })
 
   
